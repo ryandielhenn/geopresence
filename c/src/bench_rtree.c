53f680a4b404cb8216ef9dadaeae726ae32d7227
Ryan Dielhenn <dielhennr@gmail.com>
2026-03-02 22:18:00 -0800

    fix: bug in competitive rtree benchmark (#2)

--- c/src/bench_rtree.c
+++ c/src/bench_rtree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "geohash.h"
 #include "rtree.h"
 #include "timer.h"
@@ -11,11 +12,10 @@ struct city {
 };
 
 /* Number of matches found in the search */
-int match_count;
+int match_count = 0;
 
 bool city_iter(const double *rect, const void *item, void *udata) {
     match_count++;
-    // This always prints 0.0 0.0 - the lat/lon values get lost somewhere?
     return true;
 }
 
@@ -23,10 +23,10 @@ bool city_iter(const double *rect, const void *item, void *udata) {
  * Generate a random double between low and high
  */
 double drand(double low, double high) {
-    return ((double)rand() * ( high - low )) / (double)RAND_MAX + low;
+    return ((double)rand() * (high - low)) / (double)RAND_MAX + low;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr,
                 "Usage: %s insertions\n"
@@ -39,8 +39,8 @@ int main(int argc, char** argv) {
 
     struct spatial_range sr;
     geohash_decodeN(&sr, "9x");
-    // create a new rtree where each item is a `struct city`. 
-    struct rtree *tr = rtree_new(sizeof(struct city), 2);
+    // create a new rtree where each item is a `struct city`.
+    struct rtree *tr = rtree_new(sizeof(struct city *), 2);
 
     for (int i = 0; i < insertions; ++i) {
         struct city *p = malloc(sizeof(struct city));
@@ -49,18 +49,15 @@ int main(int argc, char** argv) {
         p->lat = rand_y;
         p->lon = rand_x;
         p->name = "9x";
-        //uncomment to print randomly generated lat/lon coordinates in 9x
-        //printf("%f %f\n", rand_y, rand_x);
-        rtree_insert(tr, (double[]){ rand_x, rand_y, rand_x, rand_y } , p);
+        rtree_insert(tr, (double[]){rand_x, rand_y, rand_x, rand_y}, &p);
     }
 
-    // This finds none of what was inserted even though everything that was inserted
-    // should be in the bounds of this rectangle
     match_count = 0;
     double start = timer_now();
-    rtree_search(tr, (double[]){sr.west, sr.south, sr.east, sr.north}, city_iter, NULL);
+    rtree_search(tr, (double[]){sr.west, sr.south, sr.east, sr.north},
+                 city_iter, NULL);
     printf("%f\n", timer_now() - start);
-
+    printf("Found: %d matches during rtree search\n", match_count);
 
     rtree_free(tr);
 }
